079bf748955f43c30660c642aa3d323da90ff861
Oleh Vatulia <oleg.vatulya@gmail.com>
2017-09-20 23:38:00 +0300

    first commit

---
+++ src/com/shlak/Addres.java
@@ -0,0 +1,5 @@
+package com.shlak;
+
+public class Addres {
+
+}
---
+++ src/com/shlak/Menu.java
@@ -0,0 +1,14 @@
+package com.shlak;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Menu {
+    Scanner s = new Scanner(new File("Shlak.txt"));
+    ArrayList<String> list = new ArrayList<String>();
+
+    public Menu() throws FileNotFoundException {
+    }
+
+}
